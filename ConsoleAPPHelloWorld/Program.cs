ecd7dd55dfb5addbedae003aef5e63a431e37ca2
sabarirajp <sabarirajp@TRNE-SABARIP>
2025-04-22 13:57:48 +0530

    Add project files.

---
+++ ConsoleAPPHelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAPPHelloWorld
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World...!!");
+        }
+    }
+}
