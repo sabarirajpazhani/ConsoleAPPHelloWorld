8ed8493f442af9f1c6517aa5dbac125983002614
sabarirajp <sabarirajp@TRNE-SABARIP>
2025-04-22 16:47:11 +0530

    User input Console application

---
+++ ConsoleAPPHelloWorld/UserInputConsoleApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAPPHelloWorld
+{
+    public  class UserInputConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Sentence: ");
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine($"User Entered Sentence : {userInput}");
+        }
+    }
+}
